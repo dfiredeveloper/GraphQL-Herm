d3d1a73805dde328e16d2e20b56556a29cee75a2
dfiredeveloper <dfiredeveloper@gmail.com>
2020-01-27 17:36:16 +0100

    set font family

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,6 @@ import styled from 'styled-components'
 const Title = styled.h1`
   font-size: 40px;
   color: rebeccapurple;
+  font-family: 'Nunito', sans-serif;
 `
 export default () => <Title>Hello, Herm</Title>
