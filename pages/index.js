2132e7168fe3c5bcebbae9d63710db3bbc109a2c
dfiredeveloper <dfiredeveloper@gmail.com>
2020-01-27 17:55:42 +0100

    Add layout component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
-import React from 'react'
-import styled from 'styled-components'
-const Title = styled.h1`
-  font-size: 40px;
-  color: rebeccapurple;
-  font-family: 'Nunito', sans-serif;
-`
-export default () => <Title>Hello, Herm</Title>
+import React from 'react';
+import { Text } from 'herm';
+import Layout from '../components/Layout';
+function Index() {
+  return (
+    <Layout>
+      <Text fontSize="32px">Hello, Herm</Text>
+    </Layout>
+  );
+}
+export default Index;
